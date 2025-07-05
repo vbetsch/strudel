c88bd3556b449255411d7a229e192b2860bb41be
vbetsch <vic.betsch@gmail.com>
2025-07-05 16:39:47 +0200

    feat: Setup Strudel

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import { repl, s, n, stack, cps } from "@strudel/core";
+import {
+  initAudioOnFirstClick,
+  getAudioContext,
+  webaudioOutput
+} from "@strudel/webaudio";
+
+const btnPlay = document.getElementById("play");
+const btnStop = document.getElementById("stop");
+
+const ctx = getAudioContext();
+const { scheduler } = repl({
+  defaultOutput: webaudioOutput,
+  getTime: () => ctx.currentTime
+});
+
+btnPlay.addEventListener("click", async () => {
+  await initAudioOnFirstClick();            // permission audio
+
+  cps(1);                                   // règle le tempo (1 cycle/s)
+
+  const pattern = stack(
+    s("bd sd hh").gain(0.8),
+    n("<0 2>").s("bass").mode("minor")
+  );
+
+  scheduler.setPattern(pattern);
+  scheduler.start();
+});
+
+btnStop.addEventListener("click", async () => {
+  scheduler.stop();
+});
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,          // vide dist/ à chaque build
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+      inject: "body",
+    }),
+  ],
+  devServer: {
+    static: "./dist",
+    open: true,            // ouvre le navigateur
+    port: 8080,
+  },
+};
